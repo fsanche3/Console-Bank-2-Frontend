5e6634156bb6dd9ac13716126be59f4c581d4e46
fsanche3 <fsanche3@oswego.edu>
2022-09-01 19:28:10 -0400

    nav/routing set up with one bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsComponent } from './components/accounts/accounts.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
 
 const routes: Routes = [
   {
     path:'', redirectTo: 'home', 
     pathMatch: 'full'
   },
-  {path : 'home', component: HomeComponent},
+  {path : 'home',  component: HomeComponent},
+  {path : 'accounts', component: AccountsComponent},
+  {path : 'profile', component: ProfileComponent},
+  {path : 'transactions', component: TransactionsComponent},
   {path : 'login', component: LoginComponent},
   {path : 'register', component: RegisterComponent},
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
@@ -10,10 +11,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
 import { UserService } from './services/user.service';
 import { FormsModule } from '@angular/forms';
+import{MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatInputModule} from '@angular/material/input';
 import { RegisterComponent } from './components/register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +29,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HomeComponent,
     LoginComponent,
     RegisterComponent,
+    ProfileComponent,
+    AccountsComponent,
+    TransactionsComponent,
     
   ],
   imports: [
@@ -32,6 +42,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatButtonModule,
     MatInputModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    
   ],
   providers: [AuthService, UserService],
   bootstrap: [AppComponent]
