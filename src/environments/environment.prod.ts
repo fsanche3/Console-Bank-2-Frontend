c3c0b48813ac0d7182ec4c9574c84192a609d45f
fsanche3 <fsanche3@oswego.edu>
2022-09-09 21:48:39 -0400

    added ec2 and s3 need to solve mobile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: false
+  production: true,
+  withCredentials: true,
+  baseUrl: "http://ec2-34-234-73-192.compute-1.amazonaws.com:8080",
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'http://localhost:4200',
+    'Auth': "",
+  },
 };
