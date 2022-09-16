0062d4b9cb1ba9fbb5bfab252fcb30458b4b1af9
fsanche3 <fsanche3@oswego.edu>
2022-09-16 14:54:04 -0400

    Added all jasmine test for services

--- src/app/services/account.service.spec.ts
+++ src/app/services/account.service.spec.ts
@@ -2,15 +2,178 @@ import { TestBed } from '@angular/core/testing';
 
 import { AccountService } from './account.service';
 
-describe('AccountService', () => {
+fdescribe('AccountService', () => {
   let service: AccountService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers:[AccountService]
+    });
     service = TestBed.inject(AccountService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should apply intrest and return true', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:200}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.applyIntrest(0)).toBeTrue();
+  });
+
+  it('should not apply intrest and return false', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:404}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.applyIntrest(0)).toBeFalse();
+  });
+
+  it('should deposit Savings and return true', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:200}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.depositSaving(0,0)).toBeTrue();
+  });
+
+  it('should not deposit Savings and return false', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:404}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.depositSaving(0,0)).toBeFalse();
+  });
+
+  it('should deposit Checkings and return true', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:200}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.depositChecking(0,0)).toBeTrue();
+  });
+
+  it('should not deposit Checkings and return false', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:404}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.depositChecking(0,0)).toBeFalse();
+  });
+
+  it('should withdrawl Checkings and return true', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:200}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.withdrawlChecking(0,0)).toBeTrue();
+  });
+
+  it('should not withdrawl Checkings and return false', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:404}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.withdrawlChecking(0,0)).toBeFalse();
+  });
+
+  it('should withdrawl Savings and return true', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:200}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.withdrawlSaving(0,0)).toBeTrue();
+  });
+
+  it('should not withdrawl Savings and return false', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:404}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.withdrawlSaving(0,0)).toBeFalse();
+  });
+
+  it('should delete Savings and return true', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:200}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.deleteSavings(0)).toBeTrue();
+  });
+
+  it('should not delete Savings and return false', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:404}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.deleteSavings(0)).toBeFalse();
+  });
+
+  it('should delete Checkings and return true', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:200}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.deleteCheckings(0)).toBeTrue();
+  });
+
+  it('should not delete Checkings and return false', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:404}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.deleteCheckings(0)).toBeFalse();
+  });
+
+  it('should create Checkings and return true', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:200}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.createCheckings(0, "")).toBeTrue();
+  });
+
+  it('should not create Checkings and return false', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:404}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.createCheckings(0, "")).toBeFalse();
+  });
+
+  it('should create Savings and return true', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:200}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.createSavings(0, 1.0,"")).toBeTrue();
+  });
+
+  it('should not create Savings and return false', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:404}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.createSavings(0,1.0, "")).toBeFalse();
+  });
+
+  it('should return Savings accounts', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:200}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.getSavingAccounts()).toBeTruthy();
+  });
+
+  it('should return Checkings accounts', async () => {
+    let fakeResp = new Response('{"id":"test"}',
+    {status:200}
+  );
+  spyOn(window, 'fetch').and.resolveTo(fakeResp);
+  expect(await service.getCheckingAccounts()).toBeTruthy();
+  });
+
 });
--- src/app/services/transaction.service.spec.ts
+++ src/app/services/transaction.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { TransactionService } from './transaction.service';
 
-fdescribe('TransactionService', () => {
+describe('TransactionService', () => {
   let service: TransactionService;
 
   beforeEach(() => {
@@ -15,4 +15,70 @@ fdescribe('TransactionService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should set accountType to a boolean value', ()=>{
+    service.accountType = false;
+    service.setAccountType(true)
+    expect(service.accountType).toBeTrue();
+  });
+
+  it('should get accountType', ()=>{
+    service.setAccountType(true)
+    expect(service.getAccountType()).toBeTruthy();
+  });
+
+  it('should set savingId', ()=>{
+    service.setSavingId(8);
+    expect(service.savingId).toBe(8);
+  });
+
+  it('should get savingId', ()=>{
+    service.savingId = 7;
+    expect(service.getSavingId()).toBe(7);
+  });
+
+  it('should set checkingId', ()=>{
+    service.setCheckingId(8);
+    expect(service.checkingId).toBe(8);
+  });
+
+  it('should get checkingId', ()=>{
+    service.checkingId = 7;
+    expect(service.getCheckingId()).toBe(7);
+  });
+
+  it('should get checking Transactions', ()=>{
+    let fakeResp = new Response('{"id":"test"}',
+      {status:200}
+    );
+    spyOn(window, 'fetch').and.resolveTo(fakeResp);
+    expect(service.getCheckingTransaction(0)).toBeTruthy();
+  });
+
+  it('should not get checking Transactions', async ()=>{
+    let fakeResp = new Response('{"id":"test"}',
+      {status:404}
+    );
+    spyOn(window, 'fetch').and.resolveTo(fakeResp);
+
+    expect(await service.getCheckingTransaction(0)).toBeFalse();
+  });
+
+  it('should get saving Transactions', ()=>{
+    let fakeResp = new Response('{"id":"test"}',
+      {status:200}
+    );
+    spyOn(window, 'fetch').and.resolveTo(fakeResp);
+    expect(service.getSavingTransaction(0)).toBeTruthy();
+  });
+
+  it('should not get saving Transactions', async ()=>{
+    let fakeResp = new Response('{"id":"test"}',
+      {status:404}
+    );
+    spyOn(window, 'fetch').and.resolveTo(fakeResp);
+
+    expect(await service.getSavingTransaction(0)).toBeFalse();
+  });
+
 });
