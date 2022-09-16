992a14b10a74fbd84312c5cee7db14c42cc728ba
fsanche3 <fsanche3@oswego.edu>
2022-09-15 21:04:24 -0400

    edit enviremont to run on localhost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
   withCredentials: true,
  // baseUrl: "http://consolebank2-env.eba-qkrs9mmh.us-east-1.elasticbeanstalk.com",
     baseUrl: "http://localhost:8080",
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Auth': "",
-  },
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'http://localhost:4200',
+      'Auth': ""
+    },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,10 +8,10 @@ export const environment = {
  // baseUrl: "http://consolebank2-env.eba-qkrs9mmh.us-east-1.elasticbeanstalk.com",
  baseUrl: "http://localhost:8080", 
  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Auth': "",
-  },
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': 'http://localhost:4200',
+  'Auth': ""
+},
 };
 
 /*
